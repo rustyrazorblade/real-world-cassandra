026bfdfd75dfa1da9e75fe3cad125bc9e5fc723b
Jon Haddad <jon@jonhaddad.com>
2018-10-22 22:18:07 +1300

    testing google fix

--- content/ext/google-analytics-docinfoprocessor.rb
+++ content/ext/google-analytics-docinfoprocessor.rb
@@ -13,14 +13,16 @@ Asciidoctor::Extensions.register do
       at_location :footer
       process do |doc|
         next unless (ga_account_id = doc.attr 'google-analytics-account')
-        %(<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create','#{ga_account_id}','auto');
-ga('send','pageview');
-</script>)
+        %(
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-575989-8"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-575989-8');
+        </script>)
       end
     end
   end
